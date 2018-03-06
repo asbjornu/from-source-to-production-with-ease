861d67b061f38e92612617e9f34d7b5c0b21ecb4
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2018-03-07 00:45:02 +0100

    Added simple NGINX container with initial Reveal presenation

---
+++ docs/js/init.js
@@ -0,0 +1,20 @@
+Reveal.initialize({
+    width: 1920,
+    height: 1080,
+    margin: 0.1,
+    controls: false,
+    center: false,
+    dependencies: [
+        {
+            src: './bower_components/reveal.js/plugin/notes/notes.js',
+            async: true
+        },
+        {
+            src: './bower_components/reveal.js/plugin/highlight/highlight.js',
+            async: true,
+            callback: function() {
+                hljs.initHighlightingOnLoad();
+            }
+        }
+    ]
+});
