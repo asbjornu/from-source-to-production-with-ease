b7c106c2c8593d211eda2419ff09ee7d409dbb99
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2018-03-13 20:57:37 +0200

    Added more checks and bounds to the exercise script

--- exercise/exercise.sh
+++ exercise/exercise.sh
@@ -5,8 +5,15 @@ tag="exercise-$1"
 
 mkdir -p /tmp/from-source-to-production-with-ease
 cd /tmp/from-source-to-production-with-ease
-[ -d .git ] || git clone ~/Dropbox/Dev/from-source-to-production-with-ease . 2>&1 >/dev/null
-git checkout --force tags/$tag 2>&1 >/dev/null
+if [ -d .git ]; then
+  if [ $(git symbolic-ref --short -q HEAD) != "master" ]; then
+    git -c advice.statusHints=false checkout --force master 2>&1 >/dev/null
+  fi;
+  git pull 2>&1 >/dev/null
+else
+  git clone ~/Dropbox/Dev/from-source-to-production-with-ease . 2>&1 >/dev/null
+fi;
+git -c advice.detachedHead=false reset --hard tags/$tag >/dev/null
 rsync -avP \
       --exclude="exercise.sh" \
       ./exercise/ \
