23d599483531bf82a62b7f29d4938c8769497f0c
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2018-03-13 20:21:20 +0200

    Implemented the exercise script to easily jump between exercises

--- exercise/exercise.sh
+++ exercise/exercise.sh
@@ -1,19 +1,15 @@
-case $1 in
-1)
-  message="One!"
-  ;;
-2)
-  message="Two!"
-  ;;
-3)
-  message="Three!"
-  ;;
-4)
-  message="Four!"
-  ;;
-*)
-  message="Out of range"
-  ;;
-esac
+#!/bin/sh
 
-echo $message
+cwd=$(pwd)
+tag="exercise-$1"
+
+mkdir -p /tmp/from-source-to-production-with-ease
+cd /tmp/from-source-to-production-with-ease
+[ -d .git ] || git clone ~/Dropbox/Dev/from-source-to-production-with-ease . 2>&1 >/dev/null
+git checkout --force tags/$tag 2>&1 >/dev/null
+rsync -avP \
+      --exclude="exercise.sh" \
+      ./exercise/ \
+      $cwd 2>&1 >/dev/null
+
+cd $cwd
