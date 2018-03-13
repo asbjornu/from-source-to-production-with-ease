9f9c5107d6b1b48aa921d35a19a9f2b69ab684f5
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2018-03-13 21:00:18 +0200

    Perform all git operations quietly

--- exercise/exercise.sh
+++ exercise/exercise.sh
@@ -7,13 +7,13 @@ mkdir -p /tmp/from-source-to-production-with-ease
 cd /tmp/from-source-to-production-with-ease
 if [ -d .git ]; then
   if [ $(git symbolic-ref --short -q HEAD) != "master" ]; then
-    git -c advice.statusHints=false checkout --force master 2>&1 >/dev/null
+    git -c advice.statusHints=false checkout --quiet --force master 2>&1 >/dev/null
   fi;
-  git pull 2>&1 >/dev/null
+  git pull --quiet 2>&1 >/dev/null
 else
   git clone ~/Dropbox/Dev/from-source-to-production-with-ease . 2>&1 >/dev/null
 fi;
-git -c advice.detachedHead=false reset --hard tags/$tag >/dev/null
+git -c advice.detachedHead=false reset --quiet --hard tags/$tag >/dev/null
 rsync -avP \
       --exclude="exercise.sh" \
       ./exercise/ \
