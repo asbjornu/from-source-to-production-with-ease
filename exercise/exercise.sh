b5d7b84f9571563923e9236c78d6c20a738c993f
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2018-03-13 22:03:06 +0200

    Fetch and reset instead of pull, plus wipe local tags

--- exercise/exercise.sh
+++ exercise/exercise.sh
@@ -9,7 +9,10 @@ if [ -d .git ]; then
   if [ $(git symbolic-ref --short -q HEAD) != "master" ]; then
     git -c advice.statusHints=false checkout --quiet --force master 2>&1 >/dev/null
   fi;
-  git pull --quiet 2>&1 >/dev/null
+  git tag -l | xargs git tag -d 2>&1 >/dev/null
+  git fetch --quiet --tags 2>&1 >/dev/null
+  git fetch --quiet origin 2>&1 >/dev/null
+  git reset --quiet --hard origin/master 2>&1 >/dev/null
 else
   git clone ~/Dropbox/Dev/from-source-to-production-with-ease . 2>&1 >/dev/null
 fi;
