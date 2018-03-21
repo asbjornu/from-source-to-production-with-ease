f3563f82dc967bfeade2c3e2d929aa7655d8805e
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2018-03-21 12:26:15 +0100

    Added print stylesheet

--- presentation/js/init.js
+++ presentation/js/init.js
@@ -17,3 +17,11 @@ var presentation = new Presentation();
 Reveal.addEventListener('slidechanged', presentation.slideChanged);
 Reveal.addEventListener('fragmentshown', presentation.fragmentShown);
 Reveal.addEventListener('fragmenthidden', presentation.fragmentHidden);
+
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = window.location.search.match(/print-pdf/gi)
+          ? 'node_modules/reveal.js/css/print/pdf.css'
+          : 'node_modules/reveal.js/css/print/paper.css';
+document.getElementsByTagName('head')[0].appendChild(link);
